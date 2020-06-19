7ed1c0f8d45848a04f12f0f073c2f2a94523fbb1
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-19 11:23:55 +0200

    1 file modified

--- src/app/api-annonces.service.ts
+++ src/app/api-annonces.service.ts
@@ -2,12 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
-import { Annonce } from './interfaces/annonces';
-import { PostLogin } from './interfaces/post-login';
+import { Annonce } from './interfaces/annonce';
 
 @Injectable()
 export class ApiAnnoncesService {
-  constructor( private http: HttpClient, private datepipe: DatePipe,private messageService: MessageService) { }
+  constructor( private http: HttpClient, private datepipe: DatePipe) { }
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -17,16 +16,11 @@ export class ApiAnnoncesService {
   private apiUrl_AnnoncesGet = 'https://dwarves.iut-fbleau.fr/~pruvost/WebMAIRIECOVID/android_login_api/annoncesGet.php';
   private apiUrl_AnnoncesPost = 'https://dwarves.iut-fbleau.fr/~pruvost/WebMAIRIECOVID/android_login_api/annoncesPost.php';
   
-  login(mail,mdp) {
-    return this.http.get<any>(this.apiUrl_Login + '?mail=' + mail + '&mdp=' + mdp);
+  getClassifiedAd() {
+    return this.http.get<any>(this.apiUrl_AnnoncesGet);
   }
 
-  register(formData:PostLogin) : Observable<PostLogin> {
-    console.log(JSON.stringify(formData));
-    console.log(this.messageService);
-    return this.http.post<any>(this.apiUrl_Register, JSON.stringify(formData),this.httpOptions).pipe(
-      tap((newPostLogin: PostLogin) => this.log(`added utilisateur w/ mail=${newPostLogin.mail}`)),
-      catchError(this.handleError<PostLogin>('register'))
-    );
+  addClassifiedAd(formData:Annonce) {
+    return this.http.post<any>(this.apiUrl_AnnoncesPost, JSON.stringify(formData),this.httpOptions);
   }
 }
