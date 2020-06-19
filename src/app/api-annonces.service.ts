e3a82d59f559679cf2ae5d83f8583e7385fdfa33
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-19 19:10:37 +0200

    1 file modified

--- src/app/api-annonces.service.ts
+++ src/app/api-annonces.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-
+import {RequestOptions, Request, RequestMethod} from '@angular/http';
 import { DatePipe } from '@angular/common';
 import { Annonce } from './interfaces/annonce';
 import { MessageService } from './message.service';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiAnnoncesService {
