fdd61161ff9162a15e42ae7f0cf3c8e00da85776
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-20 15:56:44 +0200

    1 file modified

--- src/app/header/login/login.component.ts
+++ src/app/header/login/login.component.ts
@@ -37,7 +37,6 @@ export class LoginComponent implements OnInit {
     console.log(this.formGroup.get('mdp').value);*/
     this.apiUtilisateursService.login(this.formGroup.get('mail').value, this.formGroup.get('mdp').value).subscribe((response) => {
       this.id = response['id'];
-      console.log(response['id']);
       console.log(this.id);
       this.session = response['user'];
     }, (error) => {
