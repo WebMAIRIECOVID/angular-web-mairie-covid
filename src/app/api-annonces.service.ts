aa0c625b451ff53b69cd8ddec8d942c393ed1a4a
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-20 17:32:22 +0200

    1 file modified

--- src/app/api-annonces.service.ts
+++ src/app/api-annonces.service.ts
@@ -20,10 +20,10 @@ export class ApiAnnoncesService {
   };
 
   proxyurl = "https://cors-anywhere.herokuapp.com/";
-  //private apiUrl_AnnoncesGet = 'https://dwarves.iut-fbleau.fr/~pruvost/WebMAIRIECOVID/android_login_api/annonceGet.php';
-  //private apiUrl_AnnoncesPost = 'https://dwarves.iut-fbleau.fr/~pruvost/WebMAIRIECOVID/android_login_api/annoncePost.php';
-  private apiUrl_AnnoncesGet = 'https://dwarves.iut-fbleau.fr/~thor/android_login_api/annonceGet.php';
-  private apiUrl_AnnoncesPost = 'https://dwarves.iut-fbleau.fr/~thor/android_login_api/annoncePost.php';
+  private apiUrl_AnnoncesGet = 'https://dwarves.iut-fbleau.fr/~pruvost/WebMAIRIECOVID/android_login_api/annonceGet.php';
+  private apiUrl_AnnoncesPost = 'https://dwarves.iut-fbleau.fr/~pruvost/WebMAIRIECOVID/android_login_api/annoncePost.php';
+  //private apiUrl_AnnoncesGet = 'https://dwarves.iut-fbleau.fr/~thor/android_login_api/annonceGet.php';
+  //private apiUrl_AnnoncesPost = 'https://dwarves.iut-fbleau.fr/~thor/android_login_api/annoncePost.php';
   
   getClassifiedAd() {
     return this.http.get<any>(this.apiUrl_AnnoncesGet);
