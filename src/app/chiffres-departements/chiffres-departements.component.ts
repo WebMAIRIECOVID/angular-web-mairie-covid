9ce200e3c3f1610a18127101d5323cbd16cad1ce
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-20 22:12:13 +0200

    1 file modified

--- src/app/chiffres-departements/chiffres-departements.component.ts
+++ src/app/chiffres-departements/chiffres-departements.component.ts
@@ -13,17 +13,8 @@ export class ChiffresDepartementsComponent implements OnInit {
   constructor(private apiStatistiquesService: ApiStatistiquesService, private el:ElementRef) { }
 
   ngOnInit() {
-      this.apiStatistiquesService.getInformationsPays(this.pays).subscribe((response) => {
+      this.apiStatistiquesService.getInformationsDep().subscribe((response) => {
         
-        this.last_update = response[0]["last_update"];
-
-        this.tabFrance = [
-              { name: 'Cas', value: response[0]["cases"] },
-              { name: 'Décès', value: response[0]["deaths"] },
-              { name: 'Guérisson', value: response[0]["recovered"] }
-            ];
-        console.log(this.tabFrance);
-        Object.assign(this, this.tabFrance );
       });
   }
 
