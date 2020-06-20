d4318daaaa675393ee12c6e756c4c4aeade65cfe
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-20 15:15:44 +0200

    1 file modified

--- src/app/contenu-principal/contenu-principal-centre/annonces/ajouter-annonce/ajouter-annonce.component.ts
+++ src/app/contenu-principal/contenu-principal-centre/annonces/ajouter-annonce/ajouter-annonce.component.ts
@@ -16,7 +16,7 @@ export class AjouterAnnonceComponent implements OnInit {
     this.formGroup = new FormGroup({
       texte: new FormControl(),
       titre: new FormControl(),
-      auteur: new FormControl(SessionService.id),
+      auteur: new FormControl(this.id),
     });
     this.ajout = true;
   }
@@ -27,7 +27,7 @@ export class AjouterAnnonceComponent implements OnInit {
 
   onSubmit() {
     console.log("Annonce submitted");
-    console.log(SessionService.id);
+    console.log(this.id);
     this.apiAnnoncesService.addClassifiedAd(this.formGroup.value).subscribe((response) => {
       console.log(response);
     }, (error) => {
