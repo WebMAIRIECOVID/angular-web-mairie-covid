c09d294cd084cd1072f81f4a42e0a6c8c2ca7afa
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-22 15:55:52 +0200

    2 files modified

--- src/app/contenu-principal/contenu-principal-droite/chat/chat.component.ts
+++ src/app/contenu-principal/contenu-principal-droite/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Globals } from '../../../variablesGlobales/globals';
 
@@ -13,7 +13,32 @@ export class ChatComponent implements OnInit {
 
   constructor(public globals: Globals) { }
 
-  ngOnInit() {
+  @Input() chat;
+
+  ngOnInit() { 
+  }
+
+  closeChat() {
+    this.chat = false;
+  }
+
+  onSubmit() {
+    /*console.warn(this.formGroup.value);
+    console.log(this.formGroup.get('mail').value);
+    console.log(this.formGroup.get('mdp').value);*/
+    this.apiUtilisateursService.login(this.formGroup.get('mail').value, this.formGroup.get('mdp').value).subscribe((response) => {
+      this.id = response['id'];
+      this.session = response['user'];
+      this.changedSession(this.id,this.session);
+    }, (error) => {
+      alert('Erreur API login');
+    });
+    this.co = false;
+  }
+  
+  private changedSession(id, session) {
+    this.globals.id = id;
+    this.globals.session = session;
   }
 
 }
