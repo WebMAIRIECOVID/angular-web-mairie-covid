0b33e1167fb8160b2ddc1b5828a36096e5d5e08b
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-23 15:28:07 +0200

    1 file modified

--- src/app/contenu-principal/contenu-principal-centre/faq/faq.component.ts
+++ src/app/contenu-principal/contenu-principal-centre/faq/faq.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiChatService } from '../../../api-chat.service';
+
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiChatService: ApiChatService) {
+  }
+
+  items:any;
+  p: number = 1;
+
 
   ngOnInit() {
+    
+    this.apiChatService.getChat().subscribe((response) => {
+      console.log(response);
+      this.items = response["annonces"];
+    }, (error) => {
+      alert('Erreur API get annonces');
+    });
+    
   }
 
 }
