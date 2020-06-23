5da5b20053fa9386e93e5803f9b06af61f704911
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-23 16:33:41 +0200

    1 file modified, 1 added

--- src/app/contenu-principal/contenu-principal-centre/faq/resultat-questions/resultat-questions.component.ts
+++ src/app/contenu-principal/contenu-principal-centre/faq/resultat-questions/resultat-questions.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ApiChatService } from '../../../../api-chat.service';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-resultat-questions',
   templateUrl: './resultat-questions.component.html',
   styleUrls: ['./resultat-questions.component.css']
 })
+
 export class ResultatQuestionsComponent implements OnInit {
 
   items:any;
   p: number = 1;
 
-  constructor(private apiAnnoncesService: ApiAnnoncesService) {
+  constructor(private apiChatService: ApiChatService) {
   }
 
-  ngOnInit() {    
+
+  ngOnInit() {
+    
     this.apiChatService.getQuestion().subscribe((response) => {
       console.log(response);
       this.items = response["questions"];
     }, (error) => {
       alert('Erreur API get questions');
     });
+    
   }
-
 }
