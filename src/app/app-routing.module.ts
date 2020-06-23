9b6001555f804836e4d3e9875072d5a7ab5c0112
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-23 14:59:03 +0200

    2 files modified, 3 added, 3 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { StatsmondialesComponent } from './contenu-principal/contenu-principal-centre/statsmondiales/statsmondiales.component';
 import { AnnoncesComponent } from './contenu-principal/contenu-principal-centre/annonces/annonces.component';
 import { ContenuPrincipalCentreComponent } from './contenu-principal/contenu-principal-centre/contenu-principal-centre.component';
-import { MesuresComponent } from './contenu-principal/contenu-principal-centre/mesures/mesures.component';
+import { FaqComponent } from './contenu-principal/contenu-principal-centre/faq/faq.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: 'accueil', component: ContenuPrincipalCentreComponent },
-  { path: 'mesures', component: MesuresComponent },
+  { path: 'faq', component: FaqComponent },
   { path: 'statsmondiales', component: StatsmondialesComponent },
   { path: 'annonces', component: AnnoncesComponent }
 ]; 
--- src/app/constantes/item-menu.ts
+++ src/app/constantes/item-menu.ts
@@ -1,7 +1,7 @@
 import { Menu } from '../interfaces/menu';
 
 export const ITEMSMENU: Menu[] = [
-  { title: 'Foire aux questions', route: 'fa' },
+  { title: 'Foire aux questions', route: 'faq' },
   { title: 'Statistiques Mondiales', route: 'statsmondiales' },
   { title: 'Annonces des travailleurs locaux', route: 'annonces' }
 ];
