4deb2776c544ca6c0375268ca6b50814736c275b
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-25 16:13:18 +0200

    2 files modified

--- src/app/contenu-principal/contenu-principal-centre/actus/actus.component.ts
+++ src/app/contenu-principal/contenu-principal-centre/actus/actus.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActusComponent implements OnInit {
 
+  information:String;
   constructor() { }
 
   ngOnInit() {
+  this.information="information";
   }
 
 }
--- src/app/contenu-principal/contenu-principal-centre/annonces/annonces.component.ts
+++ src/app/contenu-principal/contenu-principal-centre/annonces/annonces.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnoncesComponent implements OnInit {
 
+  annonce:String;
   constructor() { }
 
   ngOnInit() {
+    this.annonce = "annonce";
   }
 
 }
