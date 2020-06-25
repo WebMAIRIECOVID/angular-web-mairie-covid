f7c3ae78ce531a8f15424472b6ba9f16a658d2ef
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-25 15:42:59 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { StatsmondialesComponent } from './contenu-principal/contenu-principal-centre/statsmondiales/statsmondiales.component';
 import { AnnoncesComponent } from './contenu-principal/contenu-principal-centre/annonces/annonces.component';
-import { ContenuPrincipalCentreComponent } from './contenu-principal/contenu-principal-centre/contenu-principal-centre.component';
+import { Actus } from './contenu-principal/contenu-principal-centre/actus/actus.component';
 import { FaqComponent } from './contenu-principal/contenu-principal-centre/faq/faq.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
-  { path: 'accueil', component: ContenuPrincipalCentreComponent },
+  { path: 'accueil', component: Actus },
   { path: 'faq', component: FaqComponent },
   { path: 'statsmondiales', component: StatsmondialesComponent },
   { path: 'annonces', component: AnnoncesComponent }
