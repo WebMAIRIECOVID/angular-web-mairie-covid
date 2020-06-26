4f9d93e223816838ef166f1ca6fac510138386a8
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-26 17:09:00 +0200

    5 files modified

--- src/app/header/register/register.component.ts
+++ src/app/header/register/register.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiUtilisateursService } from '../../api-utilisateurs.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -10,12 +21,35 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   formGroup:FormGroup;
+
+  identifiant:FormControl;
+  email:FormControl;
+  password:FormControl;
+  cat:FormControl;
   constructor(private apiUtilisateursService: ApiUtilisateursService) { 
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]);
+    this.password = new FormControl('', [
+      Validators.required
+    ]);
+    this.cat = new FormControl('', [
+      Validators.required
+    ]);
+    this.identifiant = new FormControl('', [
+      Validators.required
+    ]);
     this.formGroup = new FormGroup({
-      pseudo: new FormControl(),
-      mail: new FormControl(),
-      mdp: new FormControl(),
-      categorie: new FormControl(),
+      mail: this.email,
+      mdp: this.password,
+    });
+
+    this.formGroup = new FormGroup({
+      pseudo: this.identifiant,
+      mail: this.email,
+      mdp: this.password,
+      categorie: this.cat,
     });
   }
   @Input() ins;
@@ -39,6 +73,7 @@ export class RegisterComponent implements OnInit {
     this.ins = false;
   }
 
+  matcher = new MyErrorStateMatcher();
   /*
 
   onSubmit() {
