c17cbf423c8c182991230745f5c8ad49fbb035c8
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-30 16:32:20 +0200

    4 files modified

--- src/app/contenu-principal/contenu-principal-centre/faq/resultat-questions/ajouter-reponse/ajouter-reponse.component.ts
+++ src/app/contenu-principal/contenu-principal-centre/faq/resultat-questions/ajouter-reponse/ajouter-reponse.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
 import { ApiChatService } from '../../../../../api-chat.service';
 import { SessionService } from '../../../../../session.service';
 import { Reponse } from '../../../../../interfaces/reponse';
 import { Question } from '../../../../../interfaces/question';
 import { Globals } from '../../../../../variablesGlobales/globals';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-ajouter-reponse',
@@ -18,10 +29,17 @@ export class AjouterReponseComponent implements OnInit {
   @Input() ajout: boolean;
   reponse: Reponse;
   public globals: Globals;
+  text:FormControl;
+
+  submitted = false;
+
 
   constructor(private apiChatService: ApiChatService, private sessionService: SessionService, globals: Globals) { 
+    this.text = new FormControl('', [
+      Validators.required
+    ]);
     this.formGroup = new FormGroup({
-      texte: new FormControl(),
+      texte: this.text
     });
     this.ajout = true;
     this.globals = globals;
@@ -31,7 +49,15 @@ export class AjouterReponseComponent implements OnInit {
     this.ajout = false;
   }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.formGroup.controls; }
   onSubmit() {
+    this.submitted = true;
+    
+    // stop here if form is invalid
+    if (this.formGroup.invalid) {
+        return;
+    }
     console.log("Reponse submitted");
     console.log(this.item[0]);
     this.reponse = { texte: this.formGroup.get('texte').value, auteur: this.globals.id, question: this.item[0]};
@@ -43,6 +69,11 @@ export class AjouterReponseComponent implements OnInit {
     });
     this.ajout = false;
   }
+  
+  onReset() {
+      this.submitted = false;
+      this.formGroup.reset();
+  }
 
   closeForm() {
     this.ajout = false;
@@ -51,4 +82,5 @@ export class AjouterReponseComponent implements OnInit {
   openForm() {
     this.ajout = true;
   }
+  matcher = new MyErrorStateMatcher();
 }
