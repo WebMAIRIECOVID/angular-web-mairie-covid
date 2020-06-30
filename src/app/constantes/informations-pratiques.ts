4eb5d0065ea30498cad81bc79a0829ee1e1e4553
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-06-30 15:34:05 +0200

    2 files modified

--- src/app/constantes/informations-pratiques.ts
+++ src/app/constantes/informations-pratiques.ts
@@ -2,6 +2,5 @@ export const INFOSPRATIQUES: String[] = [
   'Corbeil-Essonnes est une commune française située à vingt-neuf kilomètres au sud-est de Paris.',
   'Mairie : 2 place Galignani 91100 Corbeil-Essonnes',
   'Horaires : du lundi au vendredi de 8h30 à 12h et de 13h30 à 17h ',
-  'Maire : Jean-Pierre Bechter (Les Républicains)',
-  'Cellule téléphonique d\'écoute et d\'information de la Mairie de Corbeil-Essonnes au 01 60 89 72 01 (24h sur 24, 7j sur 7)'
+  'Maire : Jean-Pierre Bechter (Les Républicains)'
 ];
--- src/app/constantes/liste-infos-importantes.ts
+++ src/app/constantes/liste-infos-importantes.ts
@@ -2,5 +2,6 @@ import { InfosImportantes } from '../interfaces/informations-importantes';
 
 export const LISTEINFOSIMPORTANTES: InfosImportantes[] = [
   { title: 'Alerte COVID-19', src: 'https://www.agglo-agen.net/fileadmin/_processed_/5/a/csm_corona-alert_4c2de5ecad.png', description: ' ', destination: 'https://www.gouvernement.fr/info-coronavirus' },
-  { title: 'Distribution de masques', src: 'https://www.corbeil-essonnes.fr/wp-content/uploads/Une-distribution-masques.jpg', description: '200 000 masques lavables distribués à Corbeil-Essonnes de mai à juin – Inscriptions à partir du 11 mai', destination: 'https://www.corbeil-essonnes.fr/actualites/200-000-masques-lavables-distribues-a-corbeil-essonnes-de-mai-a-juin-inscriptions-a-partir-du-11-mai/' }
+  { title: 'Le site officiel', src: 'https://www.corbeil-essonnes.fr/wp-content/themes/corbeil-essonnes/images/logos/logo.png', description: 
+  'Cellule téléphonique d\'écoute et d\'information de la Mairie de Corbeil-Essonnes au 01 60 89 72 01 (24h sur 24, 7j sur 7)', destination: 'https://www.corbeil-essonnes.fr/' }
 ];
