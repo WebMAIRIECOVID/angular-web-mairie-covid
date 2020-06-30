04987ff23c14cfaa754d1ff9ed027f677e59a174
WebMAIRIECOVID <webmairiecovid@gmail.com>
2020-07-01 00:45:32 +0200

    1 file modified

--- src/app/variablesGlobales/actualisation.ts
+++ src/app/variablesGlobales/actualisation.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
+import { ResultatQuestionsComponent } from '../contenu-principal/contenu-principal-centre/faq/resultat-questions/resultat-questions.component';
+import { ResultatAnnoncesComponent } from '../contenu-principal/contenu-principal-centre/annonces/resultat-annonces/resultat-annonces.component';
+import { ResultatReponsesComponent } from '../contenu-principal/contenu-principal-centre/faq/resultat-questions/resultat-reponses/resultat-reponses.component';
 
 @Injectable()
 export class Actualisation {
   resA: ResultatAnnoncesComponent;
   resQ: ResultatQuestionsComponent;
+  resR: ResultatReponsesComponent;
 }
